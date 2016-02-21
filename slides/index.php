4256903c932ec2a1d4369c6ae4103a2159fa43b7
kaotil <kaori.maeda@gmail.com>
2016-02-21 15:49:29 +0900

    スライドタイトル変更とスライド一覧をjsonから取得

--- slides/index.php
+++ slides/index.php
@@ -1,1 +1,9 @@
-<a href="20160216">2016/02/16</a>
+<?php
+$file = file_get_contents('./list.json', FILE_USE_INCLUDE_PATH);
+$list = json_decode($file, true);
+
+foreach ($list as $key => $val) {
+    echo "{$val['id']}: <a href='{$val['id']}'>{$val['title']}</a><br>";
+}
+?>
+
